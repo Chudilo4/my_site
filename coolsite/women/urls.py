af0fac329f22e3872d9687c6817beb5ba219876e
chudilo4 <chipalinchik@gmail.com>
2022-11-08 17:56:36 +0500

    step admin panel

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('addpage/', views.addpage, name='addpage'),
+    path('login/', views.login, name='login'),
+    path('post/<int:post_id>', views.showpost, name='post'),
+    path('category/<int:cat_id>/', views.show_category, name='category'),
 ]
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,19 +1,54 @@
 from django.shortcuts import render
 from .models import *
+from django.http import HttpResponse, Http404
 # Create your views here.
-menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+menu = [{'title':'О сайте', 'url_name': 'about'},
+        {'title': 'Добавить статью', 'url_name': 'addpage'},
+        {'title': 'Обратная связь', 'url_name': 'contact'},
+        {'title': 'Войти', 'url_name': 'login'}
+        ]
 
 
 def index(request):
     post = Women.objects.all()
+    cats = Category.objects.all()
     return render(request, 'index.html', {'title': 'Главная страница',
                                           'menu': menu,
-                                          'post': post
+                                          'posts': post,
+                                          'cats': cats,
+                                          'cat_selected': 0,
                                           })
 
 
 def about(request):
-    return  render(request, 'about.html', {'title': 'О сайте',
+    return render(request, 'about.html', {'title': 'О сайте',
                                           'menu': menu
                                            })
 
+def addpage(request):
+    return HttpResponse('Добавить страницу')
+
+
+def login(request):
+    return HttpResponse('Войти')
+
+
+def contact(request):
+    return HttpResponse('Связаться с нами!')
+
+
+def showpost(request, post_id):
+    return HttpResponse(f'Выведи пост с id = {post_id}!')
+
+
+def show_category(request, cat_id):
+    post = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+    if  len(post) == 0:
+        raise Http404()
+    return render(request, 'index.html', {'title': 'Отоброжение по рубрикам',
+                                          'menu': menu,
+                                          'posts': post,
+                                          'cats': cats,
+                                          'cat_selected': cat_id,
+                                          })
